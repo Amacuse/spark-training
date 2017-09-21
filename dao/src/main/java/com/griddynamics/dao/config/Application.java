05810007abacda4fb1979d071a948157985d4a31
iberezutskyi <iberezutskyi@griddynamics.com>
2017-09-21 15:58:41 +0300

    Minor changes - Application.java

--- dao/src/main/java/com/griddynamics/dao/config/Application.java
+++ dao/src/main/java/com/griddynamics/dao/config/Application.java
@@ -2,11 +2,9 @@ package com.griddynamics.dao.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
-@ComponentScan(value = "com.griddynamics.dao")
+@SpringBootApplication(scanBasePackages = "com.griddynamics.dao")
 @PropertySource(value = "classpath:application.properties")
 public class Application {
 
